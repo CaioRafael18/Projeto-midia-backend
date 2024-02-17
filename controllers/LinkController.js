372d730c2f8b5041618a6ad81f8f24b9a5e268fd
Caio Rafael <caiorafaelbelarmino1@gmail.com>
2024-02-17 10:29:34 -0300

    Inserindo a opção de alterar um item

--- controllers/LinkController.js
+++ controllers/LinkController.js
@@ -3,35 +3,71 @@ const Links = require("../models/LinkData")
 module.exports = {
     // Exibe a lista de links
     async read(request, response){
-        const linkLista = await Links.find()
-        return response.json(linkLista)
+        try {
+            const linkLista = await Links.find()
+            return response.json(linkLista)
+        } catch (error) {
+            return response.status(404).json({error: "Nenhum registro foi encontrado!"})
+        }
+
     },
 
     // Cria um novo item
     async create(request, response) {
         const {title, descricao, mes} = request.body
-
-        if(!title || !descricao || !mes){
-            return response.status(400).json({error: "Campo Não Preenchido!"})
+        try {
+            if(!title || !descricao || !mes){
+                return response.status(400).json({error: "Campo Não Preenchido!"})
+            }
+    
+            const criarLink = await Links.create({
+                title,
+                descricao,
+                mes
+            })
+            
+            return response.json(criarLink)
+            
+        } catch (error) {
+            return response.status(401).json({error: "Não foi possivel criar o novo registro!"})
         }
-
-        const criarLink = await Links.create({
-            title,
-            descricao,
-            mes
-        })
-        
-        return response.json(criarLink)
     },
 
+    // Deleta um item
     async delete(request, response){
         const {id} = request.params
         const deletarLink = await Links.findByIdAndDelete({_id: id})
 
-        if (deletarLink){
-            return response.json(deletarLink)
-        }
+        try {
+            if (deletarLink){
+                return response.json(deletarLink)
+            }
 
-        return response.status(401).json({error: "Não foi encontrado Registro para ser deletado!"})
+        } catch (error) {
+            return response.status(401).json({error: "Não foi encontrado Registro para ser deletado!"})
+        }
+    },
+
+    //Altera um item
+    async update(request,response){
+        const {id} = request.params
+        const {title, descricao, mes} = request.body
+
+        try {
+            if(!title || !descricao || !mes){
+                return response.status(400).json({error: "Campo Não Preenchido!"})
+            }
+
+            const linkAtualizado = await Links.findByIdAndUpdate(id, {title,descricao,mes}, {new: true})
+
+            if(!linkAtualizado){
+                return response.status(404).json({error: "Registro não encontrado!"})
+            }
+
+            return response.json(linkAtualizado)
+            
+        } catch (error) {
+            return response.status(500).json({error: "Erro ao fazer do servidor ao atualizar o registro"})
+        }
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,11 @@
 const express = require("express")
 const routes = express.Router()
 const LinkController = require("../controllers/LinkController");
-const MesController = require("../controllers/MesController")
 
 // Link
 routes.get("/links", LinkController.read)
 routes.post("/links", LinkController.create)
 routes.delete("/links/:id", LinkController.delete)
-
-// Mes
-routes.get("/mes", MesController.read)
-
-
+routes.put("/links/:id", LinkController.update)
 
 module.exports = routes
